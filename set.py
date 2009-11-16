37cd22118b45b98bab969be452c38e6bded5c1b3
Arnold Noronha <arnstein87@gmail.com>
2009-11-16 14:00:27 -0500

    First commit, with union and diff code.

---
+++ set.py
@@ -0,0 +1,155 @@
+class Set:
+    i=[]
+    def fix (self): 
+        if (len (self.i) < 2): return 
+        i = self.i
+        i.sort()
+        result = []
+        cur = i[0]
+        i = i [1:]
+
+        while (len (i) > 0):
+            if (cur [1] >= i[0][0]):
+                cur = (cur[0], max(i[0][1], cur[1]))
+            else:
+                result.append (cur)
+                cur = i[0]
+            i = i[1:]
+
+        result.append (cur)
+        self.i = result
+
+    def empty (self):
+        i = self.i
+        result = []
+        cur = i[0]
+
+        while(len(i) > 0):
+            if (cur[0] != cur[1]):
+                result.append (cur)
+            i = i [1:]
+            if (len(i) != 0):
+                cur = i[0]
+        self.i = result
+        
+    def union(self,b):
+        self.i += b.i
+        self.fix ()
+
+    def diff(self,b):
+        result = []
+        i = self.i
+        cura = i[0]
+        curb = b.i[0]
+
+        while ( len (i)):
+
+            if (cura[0]>curb[1]):
+                b.i = b.i[1:]
+                if (len(b.i) > 0):
+                    curb = b.i[0]    
+                else:
+                    result.append (cura)
+                    i = i[1:]
+                    if (len (i)>=1):
+                        cura=i[0]
+
+            if ((curb[0] > cura[0]) & (curb[1] < cura[1])):
+                curat = (cura[0], curb[0])
+                result.append (curat)
+                cura = (curb[1],cura[1])
+                result.append (cura)
+                
+                b.i = b.i[1:]
+                if (len(b.i) > 0):
+                    curb = b.i[0]    
+                i = i[1:]
+                if (len (i)>=1):
+                        cura=i[0]
+        
+            else:
+                if (cura[1]<curb[0]):
+                    result.append (cura)
+                
+                if ((cura [1] >= curb[0])&(cura[0]<=curb[0])):
+                    cura = (cura[0], curb[0])
+                    result.append (cura)
+                    
+                if ((cura [0] <= curb[1])&(cura[1]>=curb[1])):
+                    cura = (curb[1], cura[1])
+                    result.append (cura)
+
+                i = i[1:]
+                if (len (i)>=1):
+                        cura=i[0]
+        self.i = result
+        self.empty ()                
+                
+    def checkempty (self):
+        assert self.i == []        
+
+    def subset (self):
+        print self.i
+        print b.i
+        self.union (b)
+        print self.i
+        self.diff (b)
+        print self.i
+        self.checkempty ()
+
+
+def unit_test ():
+    s = Set ()
+    s.i = [(1,2), (2,3), (4,5)]
+    s.fix ()
+    print s.i 
+    assert s.i == [(1,3), (4,5)]
+
+    s.i = [(1,2), (2,3) , (3,4), (5,6), (6,7)]
+    s.fix()
+    print s.i
+    assert s.i == [(1,4), (5,7)]
+
+    s.i = [(2,4), (1,5)]
+    s.fix ()
+    print s.i
+    assert s.i == [(1,5)]
+     
+    s.i = [(1,5), (2, 5), (2,8)]
+    s.fix ()
+    print s.i
+    assert s.i == [(1,8)]
+    
+    s.i = [(1, 3), (5, 7)]
+    b = Set ()
+    b.i = [(6, 7)]
+    s.diff (b)
+    print s.i
+    assert s.i == [(1,3),(5,6)]
+
+    s.i = [(1, 3), (5, 7),(9,14)]
+    b = Set ()
+    b.i = [(6, 7),(10,12)]
+    s.diff (b)
+    print s.i
+    assert s.i == [(1,3),(5,6),(9,10),(12,14)]
+
+    s.i = [(1, 3), (5, 7)]
+    b = Set ()
+    b.i = [(1, 3), (5, 7)]
+    s.diff (b)
+    print s.i
+    assert s.i == []
+
+    s.i = []
+    s.checkempty ()
+    print s.i
+    assert s.i == []
+
+#    s.i = [(1, 2), (6, 7)]
+ #   b = Set ()
+  #  b.i = [(1, 3), (5, 7)]
+   # s.subset (b)
+    #print s.i
+
+
