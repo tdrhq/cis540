04c8cd7729ac21ca8aef66d65663fc17325123f8
Arnold Noronha <arnstein87@gmail.com>
2009-11-18 22:15:10 -0500

    part6 too.

--- partb.py
+++ partb.py
@@ -6,24 +6,30 @@ def part1(s):
     s.sumofset (Set ([(-0,1, 0.1)]))
     return s
 
+def part6 (s):
+    s.square ()
+    s.product (0.5)
+    s.sumofset (Set ([(0, 0.1)]))
+    return s
+
 def func (s):
     return part1 (s)
 
-def bfs (s, N):
+def bfs (s, N, ff):
     Reach = s
 
     for k in range (N):
-        ReachP = func (Reach)
+        ReachP = ff (Reach)
         ReachCopy = ReachP
         ReachCopy.diff (Reach)
         
         if (ReachCopy.empty ()):
             break
 
-        Reach = ReachP
-
+        Reach.union (ReachP)
 
     print Reach.i
 
 
-bfs (Set([(1,2)]), 100)
+bfs (Set([(1,2)]), 100, part1)
+bfs (Set([(1.8, 1.9)]), 2, part6)
