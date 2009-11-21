e4f9a1b2cc8fc14dc2954575891766be1173ba74
unknown <Ritika@.(none)>
2009-11-21 15:07:42 -0500

    with plot commands

--- partb.py
+++ partb.py
@@ -1,4 +1,4 @@
-
+from pylab import *
 from set import *
 
 def part1(s):
@@ -22,6 +22,11 @@ def part4 (s):
     s.sumofset (Set ([(0, 0.1)]))
     return s
 
+def part5 (s):
+    s.product (0.9)
+    s.sumofset (Set ([(-0.01, 0.01)]))
+    return s
+
 def func (s):
     return part1 (s)
 
@@ -37,12 +42,18 @@ def bfs (s, N, ff):
             break
 
         Reach.union (ReachP)
+        plot(k, Reach)
+        xlabel('k')
+        ylabel('Reach')
+        title('part5')
+        hold()
 
     print Reach.i
+    show()
 
-
-bfs (Set([(1,2)]), 100, part1)
-bfs (Set([(1,2)]), 100, part2)
-bfs (Set([(1,2)]), 100, part3)
-bfs (Set([(1.8, 1.89)]), 40, part4)
-bfs (Set([(1.8, 1.9)]), 40, part4)
+##bfs (Set([(1,2)]), 100, part1)
+##bfs (Set([(1,2)]), 100, part2)
+##bfs (Set([(1,2)]), 100, part3)
+##bfs (Set([(1.8, 1.89)]), 40, part4)
+##bfs (Set([(1.8, 1.9)]), 40, part4)
+bfs (Set([(1,2),(4,5)]), 23, part5)
